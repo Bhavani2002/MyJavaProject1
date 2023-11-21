9e5518910327bb258044eaabe68ebac86a735b0a
Bhavani2002 <kadavergubhavani@gmail.com>
2023-11-21 11:19:33 +0530

    welcome class file is updated

--- src/com/edubridge/app1/Welcome.java
+++ src/com/edubridge/app1/Welcome.java
@@ -4,6 +4,7 @@ public class Welcome {
 	public static void main(String[] args) {
 		System.out.println("welcome to java");
 		System.out.println("welcome to python");
+		System.out.println("welcome to  Angular");
 	}
 
 }
