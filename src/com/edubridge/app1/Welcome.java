5385cf3ca1c0c9acc66cc34c4657d59ad0767ce4
Bhavani2002 <151497866+Bhavani2002@users.noreply.github.com>
2023-11-21 11:10:47 +0530

    Update Welcome.java

--- src/com/edubridge/app1/Welcome.java
+++ src/com/edubridge/app1/Welcome.java
@@ -3,6 +3,7 @@ package com.edubridge.app1;
 public class Welcome {
 	public static void main(String[] args) {
 		System.out.println("welcome to java");
+		System.out.println("welcome to python");
 	}
 
 }
